9518926b571026b295a5bdbe87e063489ef63bba
NuvandaPV <jp@hhmn.de>
2023-02-26 01:07:20 +0100

    Add license notices.

--- date-progress.php
+++ date-progress.php
@@ -11,6 +11,20 @@ License:        GPL3
 License URI:    https://www.gnu.org/licenses/gpl-3.0.html
 */
 
+/*
+This file is part of DateProgress by valo.media.
+
+DateProgress is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+version.
+
+DateProgress is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with Foobar. If not, see
+<https://www.gnu.org/licenses/>.
+ */
+
 $DATE_PROGRESS_PRODUCT_ID = 'eQWGX2AwQtdGU_fQIEGsgA==';
 $DATE_PROGRESS_WATERMARK = '
 	<p>
