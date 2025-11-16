1bbdafbde34fb9569875bd40560963dc6a01e5c2
Jean-Pierre Höhmann <jean-pierre@hoehmann.jp>
2025-11-16 16:41:43 +0100

    Add copyright notices.

--- plugin.php
+++ plugin.php
@@ -1,7 +1,9 @@
 <?php
 
 /*
- * This file is part of DateProgress by valo.media.
+ * Copyright (c) 2025.
+ * valo.media GmbH
+ * All rights reserved.
  *
  * DateProgress is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
  * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
